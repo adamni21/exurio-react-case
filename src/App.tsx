98451ad0712907e82315080ed65499e8503b78f3
adamni21 <85580454+adamni21@users.noreply.github.com>
2022-08-05 14:08:23 +0100

    App: implemented ProductsGrid

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,62 @@
-import { MantineProvider, Text, Button, Stack } from "@mantine/core";
+import {
+  MantineProvider,
+  Title,
+  Text,
+  AppShell,
+  Header,
+  Navbar,
+  MediaQuery,
+  Burger,
+} from "@mantine/core";
+import { useState } from "react";
 import { theme } from "./theme";
+import ProductsProvider from "./contexts/ProductsContext";
+import ProductsGrid from "./ProductsGrid/ProductsGrid";
 
 export default function App() {
+  const [opened, setOpened] = useState(false);
   return (
     <MantineProvider theme={theme} withGlobalStyles withNormalizeCSS>
-      <Stack align="center" mt={50}>
-        <Text size="xl" weight={500}>
-          Welcome to Mantine!
-        </Text>
-        <Button>Click the button</Button>
-      </Stack>
+      <ProductsProvider>
+        <AppShell
+          navbarOffsetBreakpoint="sm"
+          asideOffsetBreakpoint="sm"
+          navbar={
+            <Navbar
+              p="md"
+              hiddenBreakpoint="sm"
+              hidden={!opened}
+              width={{ sm: 200, md: 230, lg: 300 }}
+            >
+              <Text>Application navbar</Text>
+            </Navbar>
+          }
+          header={
+            <Header height={90} p="sm">
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  height: "100%",
+                }}
+              >
+                <MediaQuery largerThan="sm" styles={{ display: "none" }}>
+                  <Burger
+                    opened={opened}
+                    onClick={() => setOpened((o) => !o)}
+                    size="sm"
+                    mr="xl"
+                  />
+                </MediaQuery>
+
+                <Title>Se farlige og mangelfulde produkter</Title>
+              </div>
+            </Header>
+          }
+        >
+          <ProductsGrid />
+        </AppShell>
+      </ProductsProvider>
     </MantineProvider>
   );
 }
