5717ef5f3b76449b8886f8532f88ebc94ed67de3
adamni21 <85580454+adamni21@users.noreply.github.com>
2022-08-05 22:07:06 +0100

    ProductsFilter: compliance support

--- src/ProductsFilter/ProductsFilter.tsx
+++ src/ProductsFilter/ProductsFilter.tsx
@@ -1,26 +1,29 @@
-import { Input, Stack } from "@mantine/core";
+import { Checkbox, Input, Stack } from "@mantine/core";
 import { useDebouncedValue } from "@mantine/hooks";
-import {
-  ChangeEvent,
-  ChangeEventHandler,
-  useContext,
-  useEffect,
-  useState,
-} from "react";
+import { ChangeEventHandler, useContext, useEffect, useState } from "react";
 import { ProductsContext } from "../contexts/ProductsContext";
+import getComplianceName from "../utils/getComplianceName";
 
 const ProductsFilter = () => {
   const ctx = useContext(ProductsContext);
   const [searchValue, setSearchValue] = useState(ctx.searchValue);
-  const [debounced] = useDebouncedValue(searchValue, 500);
+  const [debouncedSearch] = useDebouncedValue(searchValue, 500);
+  const [complianceIds, setComplianceIds] = useState(
+    ctx.complianceIds.map((id) => String(id))
+  );
 
   useEffect(() => {
-    ctx.setSearchValue(debounced);
-  }, [debounced]);
+    ctx.setSearchValue(debouncedSearch);
+  }, [debouncedSearch]);
 
   const searchChangeHandler: ChangeEventHandler<HTMLInputElement> = ({
     target,
   }) => setSearchValue(target.value);
+
+  const complianceChangeHandler = (value: string[]) => {
+    setComplianceIds(value);
+    ctx.setComplianceIds(value.map((id) => Number(id)));
+  };
 
   return (
     <Stack>
@@ -29,6 +32,15 @@ const ProductsFilter = () => {
         placeholder="Leder efter produkter"
         onChange={searchChangeHandler}
       />
+      <Checkbox.Group
+        value={complianceIds}
+        onChange={complianceChangeHandler}
+        orientation="vertical"
+        label="Filtrer på produkttype"
+      >
+        <Checkbox value="1" label={getComplianceName(1)} />
+        <Checkbox value="2" label={getComplianceName(2)} />
+      </Checkbox.Group>
     </Stack>
   );
 };
