82dbe884ef577e8350942d6a64510bbfc9950efc
adamni21 <85580454+adamni21@users.noreply.github.com>
2022-08-07 18:59:05 +0100

    added picture to <Product />

--- src/ProductsGrid/Product.tsx
+++ src/ProductsGrid/Product.tsx
@@ -1,5 +1,15 @@
-import { Badge, Card, Text } from "@mantine/core";
+import {
+  AspectRatio,
+  Badge,
+  Card,
+  Text,
+  Image,
+  Space,
+  BackgroundImage,
+  Container,
+} from "@mantine/core";
 import { FC } from "react";
+import { ApiBaseUrl } from "../Constants";
 import getComplianceName from "../utils/getComplianceName";
 
 interface Props {
@@ -11,6 +21,18 @@ interface Props {
 const Product: FC<Props> = ({ id, name, complianceTypeId }) => {
   return (
     <Card>
+      <Card.Section>
+        <AspectRatio ratio={4 / 3}>
+          <Image
+            src={`${ApiBaseUrl}/picture/${id}`}
+            alt={`produktbillede af ${name}`}
+            fit="cover"
+            style={{ backgroundColor: "#222" }}
+            withPlaceholder
+          />
+        </AspectRatio>
+      </Card.Section>
+      <Space h="md" />
       <Badge>{getComplianceName(complianceTypeId)}</Badge>
       <Text size="xl">{name}</Text>
     </Card>
